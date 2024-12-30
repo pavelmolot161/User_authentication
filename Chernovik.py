2b90865d0d5a41a64c3ea3e80ec05de2f2c8e4ee
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-12-30 19:21:18 +0300

    Промежуточный 19.20 30.12.24

---
+++ Chernovik.py
@@ -0,0 +1,37 @@
+
+
+### - 1) Устанaвливаем Django:
+#        Затем устанaвливаем необходимые библиотеки:
+
+### -                     >>>/ pip install django djangorestframework djangorestframework-simplejwt python-social-auth
+
+### - 2) Создаем новый проект Django:
+
+### -                     >>>/ django-admin startproject Autent_Project
+### -                     >>>/ cd Autent_Project
+
+### - 3) Создаем новое приложение:
+#                         >>>/ python manage.py startapp Autent_App
+
+### - 4) Запуск приложения:
+#                         >>>/ python manage.py runserver
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
